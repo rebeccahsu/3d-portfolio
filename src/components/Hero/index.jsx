c5babad616f80894e43c228d1b9eea445ff8f347
Rebecca <rebeccahsuhth@gmail.com>
2024-04-07 12:44:57 +0800

    update: hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import clsx from "clsx";
+import styles from './hero.module.scss';
+import Computers from "../canvas/Computers";
+
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section className={styles.heroContainer}>
+      <div>
+        <h1 className={styles.title}>
+          {`Hi, I'm Rebecca`}
+        </h1>
+        <h5 className={styles.subtitle}>
+          {`I'm a frontend developer.`}
+        </h5>
+      </div>
+
+      <div className={styles.canvasContainer}>
+        <Computers />
+      </div>
+    </section>
   )
 }
 
